aa6cd34181fb3eae973bf4bb9c38c4d215d9a068
felipecruz91 <felipecruz91@hotmail.es>
2020-12-11 21:13:46 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# k8s-hetzner-cloud
+
+```cli
+terraform init
+
+terraform plan -var="hcloud_token=..."
+
+terraform apply -var="hcloud_token=..." -auto-approve
+```
+
+```cli
+ssh -i ~/.ssh/id_rsa root@<IP_ADDRESS>
+```
---
+++ main.tf
@@ -0,0 +1,25 @@
+# Set the variable value in *.tfvars file
+# or using the -var="hcloud_token=..." CLI option
+variable "hcloud_token" {
+}
+
+# Create a new SSH key
+resource "hcloud_ssh_key" "default" {
+  name = "my-ssh-key"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+# Create a server
+resource "hcloud_server" "node" {
+  count = 3 #TODO: Define it as variable
+  name = "node${count.index}"
+  image = "ubuntu-20.04"
+  server_type = "cx11"
+  location = "nbg1"
+  ssh_keys = [hcloud_ssh_key.default.name]
+}
+
+output "instance_ip_addrs" {
+  value = [hcloud_server.node.*.ipv4_address]
+}
+
