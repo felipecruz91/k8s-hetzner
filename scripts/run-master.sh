46f47eb6df2d09278f9b56b2c801369206e5756a
Michael Ehrenreich <michael.ehrenreich@ffwagency.com>
2022-08-03 14:41:49 +0200

    feature(): add ip address for using certificate

--- scripts/run-master.sh
+++ scripts/run-master.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -x
 
-sudo kubeadm init --apiserver-advertise-address=10.0.1.5 --kubernetes-version stable-1.19
+default_network_interface=$(ip route show default | awk {'print $5'})
+public_ip_address=$(ip address show dev ${default_network_interface} | grep -w inet | awk {'print $2'} | sed -E "s#/.*##")
+
+sudo kubeadm init --apiserver-advertise-address=10.0.1.5 --kubernetes-version stable-1.19 --apiserver-cert-extra-sans=${public_ip_address}
 
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
